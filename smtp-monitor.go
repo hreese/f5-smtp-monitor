34ef043c989d22cb7dd82eff91ced99d7bfe3d23
Heiko Reese <mail@heiko-reese.de>
2017-11-11 15:19:17 +0100

    Obfuscated EICAR/GTUBE to prevent problems handling the binary. TESTAV/TESTSPAM now fail as expected.

--- smtp-monitor.go
+++ smtp-monitor.go
@@ -1,8 +1,8 @@
-//package f5smtpmonitor
 package main
 
 import (
 	"bytes"
+	"crypto/sha512"
 	"crypto/tls"
 	"flag"
 	"fmt"
@@ -30,10 +30,6 @@ type MailConfig struct {
 
 const (
 	bodyTemplateText = "To: {{.Recipient}}\r\nFrom: {{.Sender}}\r\nDate: {{.Date}}\r\nPrecedence: bulk\r\nAuto-Submitted: auto-generated\r\nMessage-ID: <{{.MessageID}}>\r\nSubject: {{.Subject}}\r\n\r\n{{.Mailbody}}\r\n"
-	// EICAR : Anti-Virus Test File (https://en.wikipedia.org/wiki/EICAR_test_file)
-	EICAR = "X5O!P%@AP[4\\PZX54(P^)7CC)7}$EICAR-STANDARD-ANTIVIRUS-TEST-FILE!$H+H*"
-	// GTUBE : Generic Test for Unsolicited Bulk Email (https://en.wikipedia.org/wiki/GTUBE)
-	GTUBE    = "XJS*C4JDBQADN1.NSBN3*2IDNEN*GTUBE-STANDARD-ANTI-UBE-TEST-EMAIL*C.34X"
 	helpText = `This smtp backend check expects two mandatory arguments:
 
 1. ip address (IPv4-mapped IPv6 address for IPv4 address, e.g. "":ffff:a.b.c.d")
@@ -55,6 +51,8 @@ The rest of the program is controlled by environment variables (defaults in pare
 
 var (
 	hasDebug     = false
+    sendEicar = false
+    sendGTube = false
 	syslogWriter io.Writer
 	bodyTemplate = template.Must(template.New("mailbody").Parse(bodyTemplateText))
 	bodyText     bytes.Buffer
@@ -64,7 +62,22 @@ var (
 		Date:      time.Now().Local().Format("Mon, _2 Jan 2006 15:04:05 -0700"),
 		Subject:   "F5 Loadbalancer Keepalive Test",
 	}
+	// XORKEY is used to obfuscate EICAR and GTUBE to prevent this monitor from being flagged as malware or spam
+    XORKEY = sha512.Sum512_256([]byte("f5-smtp-monitor"))
+	// EICAR : Anti-Virus Test File (https://en.wikipedia.org/wiki/EICAR_test_file)
+    EICAR = string(XOR([]byte{0x2d, 0x7f, 0x8a, 0x20, 0x23, 0x89, 0x60, 0x6a, 0x40, 0x27, 0xa4, 0x4e, 0x7e, 0xc2, 0x57, 0x4f, 0xfc, 0xab, 0x4b, 0xc5, 0x4d, 0xba, 0x67, 0xcd, 0x8e, 0x3d, 0x21, 0x94, 0x5e, 0xd2, 0x7c, 0x76, 0x27, 0x67, 0x96, 0x55, 0x32, 0xe2, 0x64, 0x6a, 0x42, 0x38, 0xbd, 0x53, 0x60, 0xcc, 0x46, 0x2c, 0x81, 0xd1, 0x4e, 0xc8, 0x49, 0xd9, 0x61, 0xdd, 0xf3, 0x27, 0x1a, 0xf9, 0x57, 0xde, 0x1e, 0x13, 0x3d, 0x61, 0x8d, 0x2b} , XORKEY[:]))
+	// GTUBE : Generic Test for Unsolicited Bulk Email (https://en.wikipedia.org/wiki/GTUBE)
+    GTUBE = string(XOR([]byte{0x2d, 0x0, 0x96, 0x2b, 0x30, 0x98, 0x6a, 0x6f, 0x52, 0x2d, 0xd1, 0x56, 0x60, 0xa9, 0x21, 0x34, 0x9b, 0xc1, 0x55, 0xa8, 0x4e, 0xbf, 0x6d, 0xca, 0xe9, 0x4f, 0x12, 0x9a, 0x5c, 0xcf, 0x6a, 0x75, 0x30, 0x67, 0x96, 0x55, 0x32, 0xe2, 0x64, 0x6a, 0x42, 0x38, 0xbd, 0x53, 0x60, 0xcc, 0x46, 0x57, 0x9d, 0xc1, 0x5e, 0xb6, 0x30, 0xc8, 0x77, 0xda, 0x8a, 0x4f, 0x11, 0xf1, 0x52, 0xd7, 0x15, 0x74, 0x5b, 0x79, 0xf1, 0x59} , XORKEY[:]))
 )
+
+// XOR returns (cleartext XOR key), repeating key if it is shorter than cleartext
+func XOR(cleartext, key []byte) []byte {
+	xor := make([]byte, len(cleartext))
+	for idx := 0; idx < len(cleartext); idx++ {
+		xor[idx] = cleartext[idx] ^ key[idx%len(key)]
+	}
+	return xor
+}
 
 // setup
 func init() {
@@ -76,14 +89,16 @@ func init() {
 	}
 	// extract commandline arguments
 	flag.Parse()
-	if flag.NArg() < 2 {
-		log.Fatalf("Only got %d commandline arguments, expected at least two", flag.NArg())
+	if flag.NArg() != 2 {
+		log.Fatalf("Got %d commandline arguments, expected exactly two", flag.NArg())
 	}
 	// enable syslog for debug runs
 	if os.Getenv("DEBUG") != "" && os.Getenv("DEBUG") != "0" {
 		hasDebug = true
 		syslogWriter, err = syslog.New(syslog.LOG_INFO|syslog.LOG_USER, "smtp-monitor")
+        // if syslog fails, silently discard debug output
 		if err != nil {
+            //fmt.Fprintf(os.Stderr, "Unable to create syslog writer: %s\n", err.Error())
 			syslogWriter = ioutil.Discard
 		}
 	}
@@ -111,11 +126,11 @@ func init() {
 	}
 	// trigger antivirus
 	if os.Getenv("TESTAV") != "" {
-		mailconfig.Mailbody += EICAR + "\n\r"
+		mailconfig.Mailbody += string(EICAR) + "\n\r"
 	}
 	// trigger spam detection
 	if os.Getenv("TESTSPAM") != "" {
-		mailconfig.Mailbody += GTUBE + "\n\r"
+		mailconfig.Mailbody += string(GTUBE) + "\n\r"
 	}
 	// render mail body
 	bodyTemplate.Execute(&bodyText, mailconfig)
@@ -176,7 +191,7 @@ func main() {
 	}
 	DebugLog("Sent RCPT TO %s", mailconfig.Recipient)
 
-	// Send the email body.
+	// Prepare email DATA
 	wc, err := c.Data()
 	if err != nil {
 		DebugLog(err.Error())
@@ -188,19 +203,42 @@ func main() {
 		DebugLog(err.Error())
 		os.Exit(5)
 	}
-	DebugLog("Sent DATA")
 
+    // send email DATA
 	err = wc.Close()
 	if err != nil {
-		DebugLog("Error after sending DATA: %s", err.Error())
-		os.Exit(6)
+        // signal success on AV/SPAM testing
+        switch {
+        case sendEicar == true:
+            fmt.Printf("Message containg EICAR test virus rejected after data: %s", err)
+            DebugLog("Message containg EICAR test virus rejected after data: %s", err)
+            os.Exit(0)
+        case sendGTube == true:
+            fmt.Printf("Message containg GTUBE spam test rejected after data: %s", err)
+            DebugLog("Message containg GTUBE spam test rejected after data: %s", err)
+            os.Exit(0)
+        default:
+            DebugLog("Error after sending DATA: %s", err.Error())
+            os.Exit(6)
+        }
 	}
+
+    // EICAR/GTUBE not prevented
+    switch {
+    case sendEicar == true:
+        DebugLog("Message containg EICAR test virus not rejected after data")
+        os.Exit(7)
+    case sendGTube == true:
+        DebugLog("Message containg GTUBE spam test not rejected after data")
+        os.Exit(8)
+    }
 
 	// Send the QUIT command and close the connection.
 	err = c.Quit()
 	if err != nil {
 		DebugLog(err.Error())
-		os.Exit(7)
+		os.Exit(9)
 	}
+    fmt.Println("OK")
 	DebugLog("Sent QUIT and closed connection")
 }
